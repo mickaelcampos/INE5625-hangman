d88f6557710b815b017eafbacdac63679c221fa6
mickaelcampos <camposmrc01@gmail.com>
2021-07-31 20:36:17 -0300

    feat: handle errors on client

--- src/Client.java
+++ src/Client.java
@@ -11,34 +11,48 @@ public class Client {
     public static void main(String[] args) throws UnknownHostException, IOException {
 
         String hostAddress = "127.0.0.1";
-
-        Socket client = new Socket(hostAddress, LISTENING_PORT);
-        System.out.println("O client: " + client + " se conectou ao servidor!");
-
+        Socket client = null;
         Scanner input = null;
         Scanner inputFromSocket = null;
         PrintStream output = null;
 
-        while (true) {
-            try {
-                input = new Scanner(System.in); // dados inseridos no meu terminal
-                output = new PrintStream(client.getOutputStream()); // canal para enviar ao server
-                inputFromSocket = new Scanner(client.getInputStream()); // canal para receber do servidor
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try { 
+            client = new Socket(hostAddress, LISTENING_PORT);
+            System.out.println("O client: " + client + " se conectou ao servidor!");
     
-            while (input.hasNextLine()) {
-                output.println(input.nextLine()); // envia o texto escrito no terminal do Client
-                String receivedData = inputFromSocket.nextLine(); // dados que o servidor  enviou
-                System.out.println(receivedData);
+            while (true) {
+                try {
+                    input = new Scanner(System.in); // dados inseridos no meu terminal
+                    output = new PrintStream(client.getOutputStream()); // canal para enviar ao server
+                    inputFromSocket = new Scanner(client.getInputStream()); // canal para receber do servidor
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                while (input.hasNextLine()) {
+                    output.println(input.nextLine()); // envia o texto escrito no terminal do Client
+                    String receivedData = inputFromSocket.nextLine(); // dados que o servidor  enviou
+                    System.out.println(receivedData);
+                }
             }
-    
-            output.close();
-            input.close();
-            inputFromSocket.close();
+        } catch (IOException ioe){
+            System.out.println("Erro o se conectar com o servidor!"); // possivelmente servidor off
+        } finally {
+            
+            if (client != null) {
+                client.close();
+            }
+            if (output != null) {
+                output.close();
+            }
+            if (input != null) {
+                input.close();
+            }
+            if (inputFromSocket != null) {
+                inputFromSocket.close();
+            }
+
         }
         
     }
-
 }
